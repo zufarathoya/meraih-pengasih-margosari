7180a899ec99f1062d34dfbcb34eee684602f4be
zufarathoya <zufarathoyabahar@mail.ugm.ac.id>
2025-07-16 02:58:19 +0700

    4th commit

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  basePath:"meraih-pengasih-margosari" 
+  basePath:"/meraih-pengasih-margosari" 
 };
 
 export default nextConfig;
