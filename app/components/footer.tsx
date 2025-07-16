b2eed5537ded4ce2e4ff98245f37bc6207d08cd4
zufarathoya <zufarathoyabahar@mail.ugm.ac.id>
2025-07-16 07:57:55 +0700

    5th commit

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -9,7 +9,7 @@ const Footer = () => {
       <footer className="bg-stone-800 text-stone-300 mt-20">
         <div className="container mx-auto px-4 sm:px-6 lg:px-8 py-8 text-center">
           <div className="flex justify-center content-center">
-            <Image src="/images/PNG_LOGO_PENGASIH.png" alt="University Logo" className="h-7 w-7 mx-10" width={500} height={500} />
+            <Image src={`${process.env.NODE_ENV === "production" ? "/meraih-pengasih-margosari" : ""}/images/PNG_LOGO_PENGASIH.png`} alt="University Logo" className="h-7 w-7 mx-10" width={500} height={500} />
             <p className="font-bold text-lg text-white">Meraih Pengasih</p>
           </div>
           <p className="mt-2 max-w-xl mx-auto">
