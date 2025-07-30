352c24a4ce40801493db51045cb1307d241a8e63
zufarathoya <zufarathoyabahar@mail.ugm.ac.id>
2025-07-31 03:08:18 +0700

    8th commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
+    "./app/components/**/*.{js,ts,jsx,tsx}",
+    "./app/layouts/**/*.{js,ts,jsx,tsx}",
     "./app/**/*.{js,ts,jsx,tsx}",
     "./pages/**/*.{js,ts,jsx,tsx}",
-    "./Components/**/*.{js,ts,jsx,tsx}"
+    "./components/**/*.{js,ts,jsx,tsx}"
   ],
   theme: {
     extend: {},
